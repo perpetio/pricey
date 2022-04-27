b60e30d0af96b7d7f71e9a41673309e2fec2098d
Andrjuxa <andrjuxa.ac@gmail.com>
2022-04-27 15:30:47 +0300

    Add data

--- app/src/main/java/com/perpetio/pricey/data/DataProvider.kt
+++ app/src/main/java/com/perpetio/pricey/data/DataProvider.kt
@@ -46,9 +46,23 @@ object DataProvider {
     val products = listOf(
         Product(productArticles[0], stores[0], 3,12.0, 15.0, DateManager.dateFromNow(2)),
         Product(productArticles[0], stores[1], 5,13.0, 10.0, DateManager.dateFromNow(5)),
-        Product(productArticles[0], stores[4], 4,11.0, 5.0, DateManager.dateFromNow(7)),
+        Product(productArticles[0], stores[4], 4,11.50, 5.0, DateManager.dateFromNow(7)),
         Product(productArticles[0], stores[5], 2,10.0, 30.0, DateManager.dateFromNow(4)),
-        Product(productArticles[0], stores[7], 3,8.0, 7.0, DateManager.dateFromNow(3)),
-        Product(productArticles[0], stores[8], 4,10.0, 12.0, DateManager.dateFromNow(5)),
+        Product(productArticles[0], stores[7], 3,8.99, 7.0, DateManager.dateFromNow(15)),
+        Product(productArticles[0], stores[8], 4,10.90, 12.0, DateManager.dateFromNow(5)),
+
+        Product(productArticles[1], stores[2], 3,17.90, 11.0, DateManager.dateFromNow(2)),
+        Product(productArticles[1], stores[3], 5,21.0, 14.0, DateManager.dateFromNow(3)),
+        Product(productArticles[1], stores[5], 4,30.0, 8.0, DateManager.dateFromNow(8)),
+        Product(productArticles[1], stores[7], 2,19.50, 7.0, DateManager.dateFromNow(14)),
+        Product(productArticles[1], stores[8], 3,27.0, 7.0, DateManager.dateFromNow(9)),
+        Product(productArticles[1], stores[9], 4,24.50, 10.0, DateManager.dateFromNow(1)),
+
+        Product(productArticles[2], stores[5], 3,12.20, 8.0, DateManager.dateFromNow(7)),
+        Product(productArticles[2], stores[7], 5,25.50, 10.0, DateManager.dateFromNow(11)),
+        Product(productArticles[2], stores[3], 1,40.0, 12.0, DateManager.dateFromNow(2)),
+        Product(productArticles[2], stores[2], 2,27.80, 7.0, DateManager.dateFromNow(1)),
+        Product(productArticles[2], stores[1], 3,33.90, 11.0, DateManager.dateFromNow(15)),
+        Product(productArticles[2], stores[0], 4,31.50, 14.0, DateManager.dateFromNow(4)),
     )
 }
